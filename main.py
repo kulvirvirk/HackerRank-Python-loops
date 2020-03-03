ad1ad460cb478beecc29f2dac69fc4554c33c7f5
kulvirvirk <kvirkdev@gmail.com>
2020-03-03 01:41:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Task
+# Read an integer N. For all non-negative integers i < N, print i**2 .See the sample for details.
+
+#Input Format
+# The first and only line contains the integer, N.
+
+# constraints: 
+# 1 <= N <=20
+
+# Output Format: 
+# Print N lines, one corresponding to each i 
+
+# Link - https://www.hackerrank.com/challenges/python-loops/problem 
+
+
