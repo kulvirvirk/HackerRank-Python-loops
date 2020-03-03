9cf64200dd46e2202eb659ef51a3bb757da10b45
kulvirvirk <kvirkdev@gmail.com>
2020-03-03 02:20:27 +0000

    added a for loop to output square value of N

--- main.py
+++ main.py
@@ -12,4 +12,10 @@
 
 # Link - https://www.hackerrank.com/challenges/python-loops/problem 
 
+n = int(input('enter N:'))
 
+if n >= 1 and n <= 20:
+  for i in range(0, n):
+    print(i**2)
+else: 
+  print('Invalid input, plz re-run the program!')
